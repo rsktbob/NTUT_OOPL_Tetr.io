6f2ebc93dd9ca69b1f187f9944780557434113ef
“呂凱達” <t110590038@ntut.edu.tw>
2023-03-15 20:09:37 +0800

    first commit

---
+++ Source/Game/mygame_run.cpp
@@ -0,0 +1,263 @@
+#include "stdafx.h"
+#include "../Core/Resource.h"
+#include <mmsystem.h>
+#include <ddraw.h>
+#include "../Library/audio.h"
+#include "../Library/gameutil.h"
+#include "../Library/gamecore.h"
+#include "mygame.h"
+#include <cstdlib>
+#include <ctime>
+
+using namespace game_framework;
+
+/////////////////////////////////////////////////////////////////////////////
+// 這個class為遊戲的遊戲執行物件，主要的遊戲程式都在這裡
+/////////////////////////////////////////////////////////////////////////////
+
+bool click_check(UINT nChar, CPoint point, CMovingBitmap character)
+{
+	if (point.x >= character.GetLeft() && point.x <= character.GetLeft() + character.GetWidth()
+		&& point.y >= character.GetTop() && point.y <= character.GetTop() + character.GetHeight()
+		&& nChar == VK_LBUTTON)
+	{
+		return true;
+	}
+	return false;
+}
+
+bool touch_check(CPoint point, CMovingBitmap character)
+{
+	if (point.x >= character.GetLeft() && point.x <= character.GetLeft() + character.GetWidth()
+		&& point.y >= character.GetTop() && point.y <= character.GetTop() + character.GetHeight())
+	{
+		return true;
+	}
+	return false;
+}
+
+CGameStateRun::CGameStateRun(CGame *g) : CGameState(g)
+{
+}
+
+CGameStateRun::~CGameStateRun()
+{
+}
+
+void CGameStateRun::OnBeginState()
+{
+}
+
+void CGameStateRun::OnMove()							// 移動遊戲元素
+{
+	if (phase == 2)
+	{
+		if (back_distance < 0 && back_selected)
+		{
+			back_distance += 10;
+		}
+		else if (back_distance > -40 && !back_selected)
+		{
+			back_distance -= 10;
+		}
+		back.SetTopLeft(back_distance, 80);
+		for (int i = 0; i < 4; i++)
+		{
+			if (second_menu_distance[i] > 305 && second_menu_selected[i])
+			{
+				second_menu_distance[i] -= 10;
+			}
+			else if (second_menu_distance[i] < 375 && !second_menu_selected[i])
+			{
+				second_menu_distance[i] += 10;
+			}
+			second_menu[i].SetTopLeft(second_menu_distance[i], 100 + i * 140);
+		}
+	}
+}
+
+void CGameStateRun::OnInit()  								// 遊戲的初值及圖形設定
+{
+	phase = 1;
+	sub_phase = 1;
+	srand((unsigned)time(NULL));
+
+	first_menu.LoadBitmapByString({ "resources/first_menu.bmp" });
+	first_menu.SetTopLeft(420, 340);
+
+	join.LoadBitmapByString({ "resources/join.bmp", "resources/join_done.bmp" });
+	join.SetTopLeft(420, 670);
+
+	tittle.LoadBitmapA({ "resources/tittle.bmp", "resources/tittle2.bmp" });
+	tittle.SetTopLeft(0, 0);
+
+	osk.LoadBitmapByString({ "resources/osk.bmp" });
+	osk.SetTopLeft(0, 930);
+
+	background.LoadBitmapByString({ "resources/background1.bmp",  "resources/background2.bmp", "resources/background3.bmp", "resources/background4.bmp",  "resources/background5.bmp", "resources/background6.bmp" });
+	background.SetTopLeft(0, 0);
+
+	second_menu[0].LoadBitmapByString({ "resources/40l.bmp", "resources/40l_selected.bmp" });
+	second_menu[0].SetTopLeft(375, 100);
+
+	second_menu[1].LoadBitmapByString({ "resources/blitz.bmp", "resources/blitz_selected.bmp" });
+	second_menu[1].SetTopLeft(375, 240);
+
+	second_menu[2].LoadBitmapByString({ "resources/zen.bmp", "resources/zen_selected.bmp" });
+	second_menu[2].SetTopLeft(375, 380);
+
+	second_menu[3].LoadBitmapByString({ "resources/custom.bmp", "resources/custom_selected.bmp" });
+	second_menu[3].SetTopLeft(375, 520);
+
+	back.LoadBitmapByString({ "resources/back.bmp", "resources/back_selected.bmp" });
+	back.SetTopLeft(-40, 80);
+
+	logo.LoadBitmapByString({ "resources/logo.bmp" });
+	logo.SetTopLeft(20, 850);
+
+	game_mode.LoadBitmapByString({ "resources/game_mode.bmp" });
+	game_mode.SetTopLeft(0, 940);
+}
+
+void CGameStateRun::OnKeyDown(UINT nChar, UINT nRepCnt, UINT nFlags)
+{
+	
+}
+
+void CGameStateRun::OnKeyUp(UINT nChar, UINT nRepCnt, UINT nFlags)
+{
+	
+}
+
+void CGameStateRun::OnLButtonDown(UINT nFlags, CPoint point)  // 處理滑鼠的動作
+{
+	if (phase == 1)
+	{
+		if (click_check(nFlags, point, join))
+		{
+			join.SetFrameIndexOfBitmap(1);
+			sub_phase += 1;
+		}
+	}
+	else if (phase == 2)
+	{
+		if (click_check(nFlags, point, back))
+		{
+			tittle.SetFrameIndexOfBitmap(0);
+			back.SetFrameIndexOfBitmap(0);
+			phase = 1;
+			sub_phase = 1;
+		}
+		for (int i = 0; i < 4; i++)
+		{
+			if (click_check(nFlags, point, second_menu[i]))
+			{
+				background.SetFrameIndexOfBitmap(rand() % 5 + 1);
+				phase = 3+i;
+				sub_phase = 1;
+			}
+		}
+	}
+}
+
+void CGameStateRun::OnLButtonUp(UINT nFlags, CPoint point)	// 處理滑鼠的動作
+{
+}
+
+void CGameStateRun::OnMouseMove(UINT nFlags, CPoint point)	// 處理滑鼠的動作
+{
+	if (phase == 2)
+	{
+		if (touch_check(point, back))
+		{
+			back.SetFrameIndexOfBitmap(1);
+			back_selected = true;
+		}
+		else
+		{
+			back.SetFrameIndexOfBitmap(0);
+			back_selected = false;
+		}
+
+		for (int i = 0; i < 4; i++)
+		{
+			if (touch_check(point, second_menu[i]) && !second_menu_selected[i])
+			{
+				second_menu[i].SetFrameIndexOfBitmap(1);
+				second_menu_selected[i] = true;
+			}
+			else if (!touch_check(point, second_menu[i]) && second_menu_selected[i])
+			{
+				second_menu[i].SetFrameIndexOfBitmap(0);
+				second_menu_selected[i] = false;
+			}
+		}
+	}
+}
+
+void CGameStateRun::OnRButtonDown(UINT nFlags, CPoint point)  // 處理滑鼠的動作
+{
+}
+
+void CGameStateRun::OnRButtonUp(UINT nFlags, CPoint point)	// 處理滑鼠的動作
+{
+}
+
+void CGameStateRun::OnShow()
+{
+	static int end_time = 0;
+	if (phase == 1)
+	{
+		tittle.ShowBitmap();
+
+		first_menu.ShowBitmap();
+		join.ShowBitmap();
+
+		osk.ShowBitmap();
+
+		if (sub_phase == 2)
+		{
+			sub_phase += 1;
+			end_time = clock() + 300;
+		}
+		if (sub_phase == 3)
+		{
+			if (clock() > end_time)
+			{
+				join.SetFrameIndexOfBitmap(0);
+				tittle.SetFrameIndexOfBitmap(1);
+				phase += 1;
+				sub_phase = 1;
+			}
+		}
+	}
+	else if (phase == 2)
+	{
+		background.ShowBitmap();
+		tittle.ShowBitmap();
+		back.ShowBitmap();
+		logo.ShowBitmap();
+		game_mode.ShowBitmap();
+		
+		for (int i = 0; i < 4; i++)
+		{
+			second_menu[i].ShowBitmap();
+		}
+	}
+	else if (phase == 3)
+	{
+		background.ShowBitmap();
+	}
+	else if (phase == 4)
+	{
+		background.ShowBitmap();
+	}
+	else if (phase == 5)
+	{
+		background.ShowBitmap();
+	}
+	else if (phase == 6)
+	{
+		background.ShowBitmap();
+	}
+}
